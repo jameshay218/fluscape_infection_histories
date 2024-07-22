79e25371ca95e8b741edabafc53457080a11b5af
James Hay <9113187+jameshay218@users.noreply.github.com>
2024-07-22 15:52:20 +0100

    Update plot_individual_titre_fits.R

--- manuscript/scripts/aux/plot_individual_titre_fits.R
+++ manuscript/scripts/aux/plot_individual_titre_fits.R
@@ -29,7 +29,28 @@ individuals=seq_along(use_indivs),
                                                   mu_indices=rep(1:48,each=4),
                                                   measurement_indices_by_time = rep(1:48,each=4),
                                                   par_tab=par_tab,time_key=time_key,virus_key=virus_key)
+
+
+plot_data <- p_titre_fits[[2]]
+p_titre_fits <- p_titre_fits[[1]]
+
+## Extract data for plot
+plot_data1 <- plot_data[[1]] %>% select(individual, virus, titre,lower,run, median,upper,samples,samples_label,virus_label) %>% rename(lower_obs=lower,upper_obs=upper,median_obs=median) 
+plot_data2 <-plot_data[[2]]%>% mutate(infection_time=variable/4)
+plot_data3 <- plot_data[[3]] %>% select(individual, virus, lower, median,upper,samples) %>% distinct()
+plot_data4 <- left_join(plot_data1,plot_data3, by=c("individual","virus","samples")) %>% select(-c(samples,virus)) %>%
+  rename(`Lower 95% prediction interval`=lower_obs, `Posterior median observation`=median_obs,
+         `Upper 95% prediction interval`=upper_obs, `Lower 95% CrI`=lower, `Upper 95% CrI`=upper, `Posterior median`=median,`Virus`=virus_label,`Sample time`=`samples_label`,`Repeat number`=run)
+plot_data2 <- plot_data2 %>% rename(`Sample time`=`samples_label`) %>% select(-samples) %>% rename(`Posterior probability of infection`=value)
+
+write.csv(plot_data4,"~/Documents/GitHub/fluscape_infection_histories//data/figure_data/FigS4Ai.csv",row.names=FALSE)
+write.csv(plot_data2,"~/Documents/GitHub/fluscape_infection_histories//data/figure_data/FigS4Aii.csv",row.names=FALSE)
+
 colnames(antigenic_map)[3] <- "inf_times"
+
+p_titre_fits$data %>% select(individual, samples_label, virus_label, titre, lower,lower_50,median,upper_50,upper)
+
+
 p_inf_hists <- generate_cumulative_inf_plots(inf_chain%>% filter(i %in% use_indivs) %>%
                                                left_join(plot_indiv_key) %>% select(-i) %>% 
                                                rename(i = i_new), 0, seq_along(use_indivs), ages = DOBs %>% mutate(individual=1:n()),strain_isolation_times = antigenic_map$inf_times)
@@ -50,6 +71,11 @@ p_cumu_infhist <- p_inf_hists[[1]] + scale_x_continuous(breaks=seq(1970,2015,by=
 
 p_titre_fit_comb <- (p_titre_fits+labs(tag="A") + theme(plot.tag = element_text(face="bold"))) + p_cumu_infhist + plot_layout(ncol=2,widths=c(2.5,1))
 ggsave_jah(p_titre_fit_comb,figure_wd,"titre_fits_example",width=7,height=8)
+
+p_cumu_infhist_data <- p_cumu_infhist$data %>% dplyr::select(-chain_no) %>% dplyr::mutate(`Circulation time`=(as.numeric(as.character(variable))/4)) %>% rename(`Lower 95% CrI`=lower,
+                                                                                                                                                                `Upper 95% CrI`=upper,
+                                                                                                                                                                `Posterior median cumulative infections`=`median`)
+write.csv(p_cumu_infhist_data,"~/Documents/GitHub/fluscape_infection_histories//data/figure_data/FigS4B.csv",row.names=FALSE)
 
 ## Find RMSE -- takes a long time
 if(FALSE){
